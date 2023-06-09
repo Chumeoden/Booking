0f7cc9dd09005d05ebe277cba10b0416e8b0a80f
Chumeoden <117746775+Chumeoden@users.noreply.github.com>
2023-06-09 21:54:00 +0700

    revise the entire project
    fix Mongoose path, fix package file, add folder routes

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
 import express  from "express";
 import dotenv  from "dotenv";
-import mongose  from "mongose";
 const app = express();
 dotenv.config();
 
-const connect = async()=>{
-try{
-    await mongose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB.")
-} catch (error) {
-    throw error
-}
-};
-
-mongose.connection.on("disconnected", ()=>{
-    console.log("mongoDB disconnected")
-})
-mongose.connection.on("connected", ()=>{
-    console.log("mongoDB connected")
-})
-
-app.length("/", (req, res)=>{
-    res.send("hello first repuest!")
-})
-
 app.listen(8800, ()=>{
-    connect()
     console.log("Connecting to backend");
 })
