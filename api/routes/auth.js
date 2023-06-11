8f4a1763d150127e9425ab7b46abd4db8944eb70
NEKOshirp <124141538+NEKOshirp@users.noreply.github.com>
2023-06-11 20:03:21 +0700

    Update auth.js
    Add router auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,33 @@
 import express from "express";
-
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-    res.send("Hello this is auth endpoint")
-})
-router.get('/register', (req, res)=>{
-    res.send("Hello this is auth register endpoint")
-})
+//CREATE
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+
+router.get("/find/:id", getHotel);
+//GET ALL
+
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router
